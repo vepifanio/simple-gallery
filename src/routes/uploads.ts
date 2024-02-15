a05f5418904d0bd10dfb085eb8540a1e09cbee06
vepifanio <vepifanio1@gmail.com>
2024-02-15 18:53:58 -0300

    feat: add upload to s3 in uploads route

--- src/routes/uploads.ts
+++ src/routes/uploads.ts
@@ -1,9 +1,11 @@
-import util from 'node:util';
-import fs from 'node:fs';
-import { pipeline } from 'node:stream';
+// import util from 'node:util';
+// import fs from 'node:fs';
+// import { pipeline } from 'node:stream';
 import { FastifyInstance } from 'fastify';
+import { PutObjectCommand } from '@aws-sdk/client-s3';
+import { client } from '../s3';
 
-const pump = util.promisify(pipeline);
+// const pump = util.promisify(pipeline);
 
 export async function uploadRoutes(app: FastifyInstance) {
   app.post('/uploads', async (request, reply) => {
@@ -14,9 +16,25 @@ export async function uploadRoutes(app: FastifyInstance) {
         message: 'A file must be provided.'
       });
     }
+    // await pump(dataFile.file, fs.createWriteStream(`./uploads/${dataFile.filename}`));
 
-    await pump(dataFile.file, fs.createWriteStream(`./uploads/${dataFile.filename}`));
 
-    return reply.status(201).send();
+    // TODO
+    // gerar id
+    // gerar nome do arquivo - id + filename
+
+    
+    const result = await client.send(
+      new PutObjectCommand({
+        Bucket: 'vesp-simple-gallery',
+        Key: dataFile.filename,
+        Body: dataFile.file,
+        ACL: 'public-read'
+      })
+    );
+
+    // salvar no banco de dados - id - original filename - link do S3 - createdAt
+
+    return reply.status(201).send({ result });
   });
 }
