a71441dc6730ab38d896218d62b1bd060ca33624
vepifanio <vepifanio1@gmail.com>
2024-02-19 09:47:16 -0300

    fix: changed pagination zod validation to parse string to a number

--- src/routes/list.ts
+++ src/routes/list.ts
@@ -4,12 +4,12 @@ import { images } from '../db/schemas';
 import { desc } from 'drizzle-orm';
 import z from 'zod';
 
-const ITEMS_PER_PAGE = 2;
+const ITEMS_PER_PAGE = 10;
 
 export async function list(app: FastifyInstance) {
   app.get('/', async (request, reply) => {
     const listImagesQuerySchema = z.object({
-      page: z.number().min(1).default(1)
+      page: z.coerce.number().min(1).default(1)
     });
 
     const { page } = listImagesQuerySchema.parse(request.query);
